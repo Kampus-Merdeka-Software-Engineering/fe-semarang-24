d7002a393a0162811d4bc59e8770c04aafacb48d
Luthfi Alwan Azhari <luthfialwan2@gmail.com>
2023-10-08 15:13:52 +0700

    Add Feture submit display

--- Js/logicForm.js
+++ Js/logicForm.js
@@ -11,7 +11,10 @@ function generateNumberResi() {
   const randomResi = Math.floor(Math.random() * 100000000000);
   return randomResi;
 }
-
+// clear Output
+function clearOutput() {
+  display.innerHTML = "";
+}
 //Post Database
 formCustomer.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -26,23 +29,40 @@ formCustomer.addEventListener("submit", (event) => {
   const weightValue = parseFloat(document.querySelector("#weight").value);
   const totalPrice = calculateTotalPrice(packageServiceValue, weightValue);
 
+  // alert condition if field empty
+  if (
+    !nameSenderValue ||
+    !nameRecipientValue ||
+    !emailValue ||
+    !addressValue ||
+    !cityFromValue ||
+    !cityToValue ||
+    isNaN(weightValue) ||
+    weightValue <= 0
+  ) {
+    alert("Please fill in all fields correctly before sending.");
+    return;
+  }
+
+  const submittedData = {
+    idreceipt: noResiValue,
+    namesender: nameSenderValue,
+    namerecipient: nameRecipientValue,
+    email: emailValue,
+    address: addressValue,
+    cityfrom: cityFromValue,
+    cityto: cityToValue,
+    packageservice: packageServiceValue,
+    itemweight: weightValue,
+    totalprice: totalPrice,
+  };
+
   fetch(`${baseUrl}packages`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({
-      idreceipt: noResiValue,
-      namesender: nameSenderValue,
-      namerecipient: nameRecipientValue,
-      email: emailValue,
-      address: addressValue,
-      cityfrom: cityFromValue,
-      cityto: cityToValue,
-      packageservice: packageServiceValue,
-      itemweight: weightValue,
-      totalprice: totalPrice,
-    }),
+    body: JSON.stringify(submittedData),
   })
     .then((res) => {
       if (res.ok) {
@@ -50,146 +70,58 @@ formCustomer.addEventListener("submit", (event) => {
       } else {
         alert("Add Customer Not successfully");
       }
-      console.log(res);
     })
     .catch((error) => {
       alert(`Error Massage: ${error.message}`);
+    })
+    .finally(() => {
+      display.innerHTML = `
+      <div class="parentPackageDiv">
+        <div>
+          <p>No Resi : ${submittedData.idreceipt}</p>
+        </div>
+        <div>
+          <p>Name Sender : ${submittedData.namesender}</p>
+        </div>
+        <div>
+          <p>Name Recipient : ${submittedData.namerecipient}</p>
+        </div>
+        <div>
+          <p>Email : ${submittedData.email}</p>
+        </div>
+        <div>
+          <p>Address : ${submittedData.address}</p>
+        </div>
+        <div>
+          <p>City From : ${submittedData.cityfrom}</p>
+        </div>
+        <div>
+          <p>City To : ${submittedData.cityto}</p>
+        </div>
+        <div>
+          <p>Package Service : ${submittedData.packageservice}</p>
+        </div>
+        <div>
+          <p>Item Weight : ${submittedData.itemweight}</p>
+        </div>
+        <div>
+          <p>Total Price : ${submittedData.totalprice}</p>
+        </div>
+        <div>
+          <button type="button" onclick="clearOutput()">Clear</button>
+        </div>
+      </div>
+      `;
     });
-});
-
-// For Display
-formCustomer.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  // const noResiValue = generateNumberResi();
-  const nameSenderValue = document.getElementById("nameSender").value;
-  // const nameRecipientValue = document.querySelector("#nameRecipient").value;
-  // const emailValue = document.querySelector("#email").value;
-  // const addressValue = document.querySelector("#address").value;
-  // const cityFromValue = document.querySelector("#cityFrom").value;
-  // const cityToValue = document.querySelector("#cityTo").value;
-  // const packageServiceValue = document.querySelector("#packageService").value;
-  // const weightValue = parseFloat(document.querySelector("#weight").value);
-
-  const getData = async () => {
-    const res = await fetch(`${urlPackage}/${nameSenderValue}`);
-    const data = await res.json();
-    console.log(data.result);
-    return data.result;
-  };
-  // getData();
-
-  const displayUser = async () => {
-    const payLoad = await getData();
-
-    let dataDisplay = payLoad.map((obj) => {
-      const { idreceipt, namesender } = obj;
-
-      return `
-          <div>
-            <p>No Resi : ${idreceipt} </p>
-          </div>
-          <div>
-            <p>Name : ${namesender} </p>
-          </div>
-        `;
-    });
-
-    display.innerHTML = dataDisplay;
-  };
-  displayUser();
-
-  //   alert condition if field empty
-  // if (
-  //   !nameSenderValue ||
-  //   !nameRecipientValue ||
-  //   !emailValue ||
-  //   !addressValue ||
-  //   !cityFromValue ||
-  //   !cityToValue ||
-  //   isNaN(weightValue) ||
-  //   weightValue <= 0
-  // ) {
-  //   alert("Please fill in all fields correctly before sending.");
-  //   return;
-  // }
-
-  // const parentPackageDiv = document.createElement("div");
-  // parentPackageDiv.className = "parentPackage";
-
-  // parentPackageDiv.innerHTML = `
-  //       <div>
-  //         <p>No Resi</p>
-  //         <p>:</p>
-  //         ${noResiValue}
-  //       </div>
-  //       <div>
-  //         <p>Name Sender</p>
-  //         <p>:</p>
-  //         ${nameSenderValue}
-  //       </div>
-  //       <div>
-  //         <p>Name Recipient</p>
-  //         <p>:</p>
-  //         ${nameRecipientValue}
-  //       </div>
-  //       <div>
-  //         <p>Email</p>
-  //         <p>:</p>
-  //         ${emailValue}
-  //       </div>
-  //       <div>
-  //         <p>Address</p>
-  //         <p>:</p>
-  //         ${addressValue}
-  //       </div>
-  //       <div>
-  //         <p>City From</p>
-  //         <p>:</p>
-  //         ${cityFromValue}
-  //       </div>
-  //       <div>
-  //         <p>City To</p>
-  //         <p>:</p>
-  //         ${cityToValue}
-  //       </div>
-  //       <div>
-  //         <p>Package Service</p>
-  //         <p>:</p>
-  //         ${packageServiceValue}
-  //       </div>
-  //       <div>
-  //         <p>Weight</p>
-  //         <p>:</p>
-  //         ${weightValue} kg
-  //       </div>
-  //       <div>
-  //         <p>Total Price</p>
-  //         <p>:</p>
-  //         ${calculateTotalPrice(packageServiceValue, weightValue)}
-  //       </div>
-  //       <div>
-  //         <button class="clearBtn">Clear</button>
-  //       </div>
-  //     `;
-
-  // button clear data
-  // const clearBtn = parentPackageDiv.querySelector(".clearBtn");
-  // clearBtn.addEventListener("click", () => {
-  //   parentPackageDiv.remove();
-  // });
-
-  // document.querySelector(".result_package").appendChild(parentPackageDiv);
-
-  // // Clear the form inputs
-  // document.querySelector("#nameSender").value = "";
-  // document.querySelector("#nameRecipient").value = "";
-  // document.querySelector("#email").value = "";
-  // document.querySelector("#address").value = "";
-  // document.querySelector("#cityFrom").value = "";
-  // document.querySelector("#cityTo").value = "";
-  // document.querySelector("#packageService").value = "";
-  // document.querySelector("#weight").value = "";
+  // Clear the form inputs
+  document.querySelector("#nameSender").value = "";
+  document.querySelector("#nameRecipient").value = "";
+  document.querySelector("#email").value = "";
+  document.querySelector("#address").value = "";
+  document.querySelector("#cityFrom").value = "";
+  document.querySelector("#cityTo").value = "";
+  document.querySelector("#packageService").value = "";
+  document.querySelector("#weight").value = "";
 });
 
 // //Check Resi
@@ -267,10 +199,6 @@ formTrack.addEventListener("submit", (event) => {
     display.innerHTML = dataDisplay;
   };
   displayTrack();
-
-  // const displayTrack = () => {
-  //   const load = getDataResi();
-  // };
 });
 
 // //Logic Price Per Kilogram
